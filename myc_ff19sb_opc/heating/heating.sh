1ffe74a8ab0cc0ad4b8754ac591fbc69f823d091
greg <go2432@wayne.edu>
2025-11-14 09:53:30 -0600

    minned

--- myc_ff19sb_opc/heating/heating.sh
+++ myc_ff19sb_opc/heating/heating.sh
@@ -1,2 +1,2 @@
 # need to construct mini_?.min files
-pmemd.cuda -O -i heating.mdin -p 1w4h_cphmd.prmtop -c 1w4h_min1.rst7 -r 1w4h_heating.rst7 -ref 1w4h_min1.rst7 -phmdparm ff19sb_pme.parm -phmdin prod.phmdin -o heating.mdout -x 1w4h_heating.nc
+pmemd.cuda -O -i heating.mdin -p myc_max_rcsb_rotated_cphmd.prmtop -c myc_max_rcsb_rotated_cphmd_min1.rst7 -r myc_max_rcsb_rotated_cphmd_heating.rst7 -ref myc_max_rcsb_rotated_cphmd_min1.rst7 -phmdparm ff19sb_pme.parm -phmdin prod.phmdin -o heating.mdout -x myc_max_rcsb_rotated_cphmd_heating.nc
