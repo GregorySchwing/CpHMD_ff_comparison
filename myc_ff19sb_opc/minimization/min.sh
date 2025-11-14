4375e4f3705238a2f37ff2a600827e34c1fe592e
greg <go2432@wayne.edu>
2025-11-14 09:45:31 -0600

    update naming

--- myc_ff19sb_opc/minimization/min.sh
+++ myc_ff19sb_opc/minimization/min.sh
@@ -1,4 +1,4 @@
 # need to construct mini_?.min files
 cp ../myc_max_rcsb_rotated_cphmd.rst7 .
 cp ../myc_max_rcsb_rotated_cphmd.prmtop .
-pmemd.cuda -O -i minimization1.mdin -p myc_max_rcsb_rotated_cphmd.prmtop -c myc_max_rcsb_rotated_cphmd.rst7 -r myc_max_rcsb_rotated_cphmd.rst7 -o myc_max_rcsb_rotated_cphmd.out -ref  myc_max_rcsb_rotated_cphmd.rst7 -x myc_max_rcsb_rotated_cphmd.nc
+pmemd.cuda -O -i minimization1.mdin -p myc_max_rcsb_rotated_cphmd.prmtop -c myc_max_rcsb_rotated_cphmd.rst7 -r myc_max_rcsb_rotated_cphmd_min1.rst7 -o myc_max_rcsb_rotated_cphmd_min1.out -ref  myc_max_rcsb_rotated_cphmd.rst7 -x myc_max_rcsb_rotated_cphmd_min1.nc
